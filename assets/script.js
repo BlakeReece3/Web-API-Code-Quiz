a3bfcdad83223beaaa31335e1c86f6e61d88eed3
Blake Reece <frankiereece3@gmail.com>
2023-05-01 20:19:35 -0700

    added to html and java script

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,10 @@ var start = document.querySelector('#start');
 var header = document.querySelector('#header');
 var questTitle = document.querySelector('#question-title');
 var questions = document.querySelector('#questions');
-var answers = document.querySelector('#answers');
+var answers = document.querySelector('#question-answers');
 var timer;
 var timerCount = 100;
-var time = document.querySelector('#time');
+var time = document.querySelector('#timer');
 var score = document.querySelector('#score');
 var initials = document.querySelector('#initials');
 var submit = document.querySelector('#submit');
@@ -17,47 +17,75 @@ var clear = document.querySelector('#clear-highscores');
 
 
 
-// Start Quiz
-var questions = [
-    {
-        title: 'Commonly used data types DO NOT include:',
-        choices: ['Strings', 'Booleans', 'Alerts', 'Numbers'],
-        answer: 'Alerts'
-    },
-    {
-        title: 'The condition in an if / else statement is enclosed within ____.',
-        choices: ['Quotes', 'Curly Brackets', 'Parentheses', 'Square Brackets'],
-        answer: 'Parentheses'
-    },
-    {
-        title: 'Arrays in JavaScript can be used to store ____.',
-        choices: ['Numbers and Strings', 'Other Arrays', 'Booleans', 'All of the Above'],
-        answer: 'All of the Above'
-    },
-    {
-        title: 'String values must be enclosed within ____ when being assigned to variables.',
-        choices: ['Commas', 'Curly Brackets', 'Quotes', 'Parentheses'],
-        answer: 'Quotes'
-    },
-    {
-        title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-        choices: ['JavaScript', 'Terminal / Bash', 'For Loops', 'Console.log'],
-        answer: 'Console.log'
-    }
-];
+function startQuiz() {
+    start.classList.add('hide');
+    startTime();
+    
+
+    var questions = [
+        {
+            title: 'Commonly used data types DO NOT include:',
+            choices: ['Strings', 'Booleans', 'Alerts', 'Numbers'],
+            answer: 'Alerts'
+        },
+        {
+            title: 'The condition in an if / else statement is enclosed within ____.',
+            choices: ['Quotes', 'Curly Brackets', 'Parentheses', 'Square Brackets'],
+            answer: 'Parentheses'
+        },
+        {
+            title: 'Arrays in JavaScript can be used to store ____.',
+            choices: ['Numbers and Strings', 'Other Arrays', 'Booleans', 'All of the Above'],
+            answer: 'All of the Above'
+        },
+        {
+            title: 'String values must be enclosed within ____ when being assigned to variables.',
+            choices: ['Commas', 'Curly Brackets', 'Quotes', 'Parentheses'],
+            answer: 'Quotes'
+        },
+        {
+            title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+            choices: ['JavaScript', 'Terminal / Bash', 'For Loops', 'Console.log'],
+            answer: 'Console.log'
+        }
+    ];
+}
 
 
 var currentQuestion = 0;
 
-function showQuestion() {
-    var question = questions[currentQuestion];
-    questTitle.textContent = question.title;
-    answers.innerHTML = '';
-    question.choices.forEach(function(choice) {
-        var button = document.createElement('button');
-        button.setAttribute('value', choice);
-        button.textContent = choice;
-        button.onclick = questionClick;
-        answers.appendChild(button);
-    });
+function startTimer() {
+    timer = setInterval(function() {
+        timerCount--;
+        time.textContent = timerCount;
+        if (timerCount <= 0) {
+            endQuiz();
+        }
+    }, 1000);
 }
+
+function showQuestion() {
+    header.classList.add('hide');
+    questions.classList.remove('hide');
+    questTitle.textContent = questions[currentQuestion].title;
+    answers.textContent = '';
+    // question.choices.forEach(function(choice) {
+    //     var button = document.createElement('button');
+    //     button.setAttribute('value', choice);
+    //     button.textContent = choice;
+    //     button.onclick = startQuiz;
+    //     answers.appendChild(button);
+    // });
+    for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
+        var button = document.createElement('button');
+        button.setAttribute('value', questions[currentQuestion].choices[i]);
+        button.textContent = questions[currentQuestion].choices[i];
+        button.onclick = checkAnswer;
+        answers.appendChild(button);
+    }
+}
+
+start.addEventListener('click', function() {
+    startTimer();
+    showQuestion();
+});
